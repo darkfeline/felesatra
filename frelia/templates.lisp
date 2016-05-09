3869055f5ce0d16d4679ea0dbde642757ce62663
Allen Li <darkfeline@felesatra.moe>
2016-05-08 23:24:39 -0700

    Cleanup templates

--- frelia/templates.lisp
+++ frelia/templates.lisp
@@ -23,77 +23,71 @@
 
 (defun base-template-footer ()
   "Footer for site base template."
-  (footer `(("id" . "site-footer"))
-          (address "The webmaster for this site is "
-                   (pgp-key-a "darkfeline") ".")
-          (div `(("id" . "site-copyright"))
-               (p (span `(("xmlns:dct" . "https://purl.org/dc/terms/")
-                          ("property" . "dct:title"))
-                        "Feles Atra")
-                  " by "
-                  (a `(("xmlns:cc" . "https://creativecommons.org/ns#")
-                       ("href" . "https://www.felesatra.moe")
-                       ("property" . "cc:attributionName")
-                       ("rel" . "cc:attributionURL"))
-                     "Allen Li")
-                  " is licensed under a "
-                  (a `(("rel" . "license")
-                       ("href" . "https://creativecommons.org/licenses/by-sa/4.0/"))
-                     "Creative Commons Attribution-ShareAlike 4.0 International License")
-                  ".")
-               (a `(("rel" . "license")
-                    ("href" . "https://creativecommons.org/licenses/by-sa/4.0/"))
-                  (img `(("alt" . "Creative Commons License")
-                         ("class" . "centered")
-                         ("src" . "https://i.creativecommons.org/l/by-sa/4.0/88x31.png")))))))
+  (:footer
+   :id "site-footer"
+   (:address "The webmaster for this site is "
+             (:pgp-key-a "darkfeline") ".")
+   (:div :id "site-copyright"
+         (:p "Feles Atra is licensed under a "
+             (:a :rel "license"
+                 :href "https://creativecommons.org/licenses/by-sa/4.0/"
+                 "Creative Commons Attribution-ShareAlike 4.0 International License")
+             ".")
+         (:a :rel "license"
+             :href "https://creativecommons.org/licenses/by-sa/4.0/"
+             (:img :alt "Creative Commons License"
+                   :class "centered"
+                   :src "https://i.creativecommons.org/l/by-sa/4.0/88x31.png")))))
 
 (defun base-template (&key title head-block body-block)
   "Site base template."
   (html-base-template
-    :title title
-    :head-block
-    (list (meta `(("name" . "viewport")
-                  ("content" . "width=device-width, initial-scale=1")))
-          (link `(("rel" . "stylesheet")
-                  ("type" . "text/css")
-                  ("href" . ,(abs-url "css/base.css"))))
-          (link `(("rel" . "stylesheet")
-                  ("type" . "text/css")
-                  ("href" . ,(abs-url "css/site.css"))))
-          (link `(("rel" . "icon")
-                  ("type" . "image/png")
-                  ("href" . ,(abs-url "img/site/favicon.png"))))
-          head-block)
-    :body-block (list (base-template-header)
-                      body-block
-                      (base-template-footer))))
+   :title title
+   :head-block
+   `((:meta :name "viewport"
+            :content "width=device-width, initial-scale=1")
+     (:link :rel "stylesheet"
+            :type "text/css"
+            :href (:abs-url "css/base.css"))
+     (:link :rel "stylesheet"
+            :type "text/css"
+            :href (:abs-url "css/site.css"))
+     (:link :rel "icon"
+            :type "image/png"
+            :href (:abs-url "img/site/favicon.png"))
+     ,@head-block)
+   :body-block
+   `(,@(base-template-header)
+     ,@block-block
+     ,@(base-template-footer))))
 
 (defun content-page-template (&key title metadata head-block content-block)
   "Content page template."
   (base-template
    :title title
    :head-block head-block
-   :body-block (list
-                (section
-                 (header `(("class" . "content-header"))
-                         (h1 title)
-                         (dl '()
-                             (let ((published (page-metadata-published metadata)))
-                               (when published
-                                 (list (dt "Published")
-                                       (dd published))))
-                             (let ((modified (page-metadata-modified metadata)))
-                               (when modified
-                                 (list (dt "Modified")
-                                       (dd modified))))
-                             (let ((category (page-metadata-category metadata)))
-                               (when category
-                                 (list (dt "Category")
-                                       (dd category)))))
-                         (let ((tags (page-metadata-tags metadata)))
-                           (when tags
-                             (dl '()
-                                 (dt "Tags")
-                                 (loop for tag in tags
-                                       collect (dd tag)))))))
-                content-block)))
+   :body-block
+   `(:section
+     (:header
+      :class "content-header"
+      (:h1 ,title)
+      (:dl
+       ,@(let ((published (slot-value metadata :published)))
+          (when published
+            `((:dt "Published")
+              (:dd ,published))))
+       ,@(let ((modified (slot-value metadata :modified)))
+           (when modified
+             `((:dt "Modified")
+               (:dd ,modified))))
+       ,@(let ((category (slot-value metadata :category)))
+           (when category
+             `((:dt "Category")
+               (:dd ,category)))))
+      ,@(let ((tags (slot-value metadata :tags)))
+          (when tags
+            `((:dl
+               (:dt "Tags")
+               ,@(loop for tag in tags
+                       collect `(:dd ,tag)))))))
+     ,@content-block)))
