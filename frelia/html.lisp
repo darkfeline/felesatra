7fd755105fc48300fd0a8e86845a0e84c1e23047
Allen Li <darkfeline@felesatra.moe>
2016-05-22 01:49:57 -0700

    Refactor void element handling

--- frelia/html.lisp
+++ frelia/html.lisp
@@ -1,34 +1,50 @@
 (in-package :frelia-html)
 
-(defvar *void-elements*)
+(defparameter *void-elements-list* '("area"
+                                     "base"
+                                     "br"
+                                     "col"
+                                     "embed"
+                                     "hr"
+                                     "img"
+                                     "input"
+                                     "keygen"
+                                     "link"
+                                     "meta"
+                                     "param"
+                                     "source"
+                                     "track"
+                                     "wbr"))
+(defvar *void-elements-table* (make-hash-table :test 'equal))
 
-(defun setup-void-elements ()
-  (setf *void-elements* (make-hash-table :test 'equal))
+(defun register-void-element (element)
+  (setf (gethash element *void-elements-table*) t))
+
+(defun register-void-elements (&rest elements)
   (loop
-    for element in '("area"
-                     "base"
-                     "br"
-                     "col"
-                     "embed"
-                     "hr"
-                     "img"
-                     "input"
-                     "keygen"
-                     "link"
-                     "meta"
-                     "param"
-                     "source"
-                     "track"
-                     "wbr")
-    do (setf (gethash element *void-elements*) t)))
-(setup-void-elements)
+    for element in elements
+    do (register-void-element element)))
+
+(defun init-void-elements-table ()
+  (clrhash *void-elements-table*)
+  (apply 'register-void-elements *void-elements-list*))
+(init-void-elements-table)
+
+(defun void-element-p (element-name)
+  "Check if ELEMENT-NAME is a void HTML element."
+  (gethash element-name *void-elements*))
 
 (defun render-html-element (element)
   "Render HTML element."
   (with-element (element name attrs content)
-    (if (gethash name *void-elements*)
+    (if (void-element-p name)
         (format-void-element name attrs)
         (format-element name attrs content))))
+
+(defun format-void-element (name attrs)
+  (format nil "<~A~{~^ ~A~}>"
+          name
+          (format-attrs attrs)))
 
 (defun format-element (name attrs content)
   (format nil "<~A~{~^ ~A~}>~{~A~}</~A>"
@@ -36,11 +52,6 @@
           (format-attrs attrs)
           (render-content 'render-html-element content)
           name))
-
-(defun format-void-element (name attrs)
-  (format nil "<~A~{~^ ~A~}>"
-          name
-          (format-attrs attrs)))
 
 (defun render-html (root-element)
   "Render XML document."
