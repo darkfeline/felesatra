f79f7fe4ea685a0fc04a284ad0e77f81dd6e75f6
Allen Li <darkfeline@felesatra.moe>
2016-05-28 18:27:53 -0700

    Update templates

--- frelia/templates.lisp
+++ frelia/templates.lisp
@@ -1,35 +1,72 @@
 (in-package :frelia-templates)
 
-(defun html-base-template (&key title head-block body-block)
-  "HTML base template."
-  `(:html :lang "en"
-          (:head
-           (:meta :charset "UTF-8")
-           (:title ,title)
-           ,@head-block)
-          (:body ,@body-block)))
+(defclass html-base-template ()
+  ((title :initarg :title
+          :accessor title)
+   (head-block :initarg :head-block
+               :accessor head-block)
+   (body-block :initarg :body-block
+               :accessor body-block))
+  (:documentation "HTML base template."))
 
-(defun base-template (&key title head-block body-block)
-  "Site base template."
-  (html-base-template
-   :title title
-   :head-block
-   `((:meta :name "viewport"
-            :content "width=device-width, initial-scale=1")
-     (:link :rel "stylesheet"
-            :type "text/css"
-            :href (:abs-url "css/base.css"))
-     (:link :rel "stylesheet"
-            :type "text/css"
-            :href (:abs-url "css/site.css"))
-     (:link :rel "icon"
-            :type "image/png"
-            :href (:abs-url "img/site/favicon.png"))
-     ,@head-block)
-   :body-block
-   `(,(base-header)
-     ,@body-block
-     ,(base-footer))))
+(defgeneric render (template))
+
+(defmethod head-block (template))
+
+(defmethod body-block (template))
+
+(defmethod render ((template html-base-template))
+  (let ((title (title template))
+        (head-block (head-block template))
+        (body-block (body-block template)))
+    `(:html :lang "en"
+            (:head
+             (:meta :charset "UTF-8")
+             (:title ,title)
+             ,@head-block)
+            (:body ,@body-block))))
+
+(defclass base-template (html-base-template)
+  (body-block :initarg :content-block :accessor content-block)
+  (:documentation "Site base template."))
+
+(defgeneric include-jquery-p (template))
+
+(defmethod include-jquery-p ((template base-template))
+  nil)
+
+(defgeneric include-ref-script-p (template)
+  (:documentation "Whether to include ref/footnote Javascript."))
+
+(defmethod include-ref-script-p ((template base-template))
+  nil)
+
+(defvar jquery-script
+  `(:script :src "https://code.jquery.com/jquery-2.2.4.min.js"))
+
+(defvar ref-script
+  `(:script ,(alexandria:read-file-into-string "frelia-resources/ref-script.js")))
+
+(defmethod head-block ((template base-template))
+  `((:meta :name "viewport"
+           :content "width=device-width, initial-scale=1")
+    (:link :rel "stylesheet"
+           :type "text/css"
+           :href (:abs-url "css/base.css"))
+    (:link :rel "stylesheet"
+           :type "text/css"
+           :href (:abs-url "css/site.css"))
+    (:link :rel "icon"
+           :type "image/png"
+           :href (:abs-url "img/site/favicon.png"))
+    ,@(if (include-jquery-p template) (list jquery-script) '())
+    ,@(if (include-ref-script-p template) (list ref-script) '())
+    ,(call-next-method)))
+
+(defmethod body-block ((template base-template))
+  (list (base-header)
+        (content-block template)
+        (base-footer)))
 
 (defun base-header ()
   "Header for site base template."
@@ -61,42 +98,35 @@
                     :class "centered"
                     :src "https://i.creativecommons.org/l/by-sa/4.0/88x31.png")))))
 
-(defun jquery-template ()
-  `(:script :src "https://code.jquery.com/jquery-2.2.4.min.js"))
+(defclass content-page-template (base-template)
+  ()
+  (:documentation "Content page template."))
 
-(defun ref-script-template ()
-  `(:script ,(alexandria:read-file-into-string "frelia-resources/ref-script.js")))
-
-(defun content-page-template (&key title head-block content-block)
-  "Content page template."
-  (base-template
-   :title title
-   :head-block head-block
-   :body-block
-   `((:section
-      (:header
-       :class "content-header"
-       (:h1 ,title)
-       (:eval-with-data
-        (data)
-        (let* ((page-resource (slot-value data :current-resource))
-               (metadata (slot-value page-resource :metadata)))
-          `(:dl
-            ,@(let ((published (slot-value metadata :published)))
-                (when published
-                  `((:dt "Published")
-                    (:dd ,published))))
-            ,@(let ((modified (slot-value metadata :modified)))
-                (when modified
-                  `((:dt "Modified")
-                    (:dd ,modified))))
-            ,@(let ((category (slot-value metadata :category)))
-                (when category
-                  `((:dt "Category")
-                    (:dd ,category))))
-            ,@(let ((tags (slot-value metadata :tags)))
-                (when tags
-                  `((:dt "Tags")
-                    ,@(loop for tag in tags
-                            collect `(:dd ,tag)))))))))
-      ,@content-block))))
+(defmethod content-block ((template content-page-template))
+  `((:section
+     (:header
+      :class "content-header"
+      (:h1 ,title)
+      (:eval-with-data
+       (data)
+       (let* ((page-resource (current-resource data))
+              (metadata (metadata page-resource)))
+         `(:dl
+           ,@(let ((published (published-date metadata)))
+               (when published
+                 `((:dt "Published")
+                   (:dd ,published))))
+           ,@(let ((modified (modified-date metadata)))
+               (when modified
+                 `((:dt "Modified")
+                   (:dd ,modified))))
+           ,@(let ((category (page-category metadata)))
+               (when category
+                 `((:dt "Category")
+                   (:dd ,category))))
+           ,@(let ((tags (page-tags metadata)))
+               (when tags
+                 `((:dt "Tags")
+                   ,@(loop for tag in tags
+                           collect `(:dd ,tag)))))))))
+     ,@(call-next-method))))
