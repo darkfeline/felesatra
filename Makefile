ce7d711d0be3db5f88bd662d25df65a46f5e885b
Allen Li <darkfeline@felesatra.moe>
2016-07-12 21:44:30 -0700

    Rejigger stuff

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@ LOCAL_BUILD_DIR=build_local
 
 .PHONY: build
 build:
-	python -m frelia ${BUILD_DIR}
+	python -m felesatra ${BUILD_DIR}
 
 SSH_HOST=www.felesatra.moe
 SSH_PORT=22
@@ -20,7 +20,7 @@ upload: clean build
 
 .PHONY: buildlocal
 buildlocal:
-	python -m frelia --site-url 'http://localhost:5000' ${LOCAL_BUILD_DIR}
+	python -m felesatra --site-url 'http://localhost:5000' ${LOCAL_BUILD_DIR}
 
 .PHONY: clean
 clean:
--- felesatra/__main__.py
+++ felesatra/__main__.py
@@ -1,61 +1,56 @@
-# pylint: disable=missing-docstring
+"""Frelia static site generator."""
 
 import argparse
 import logging
-import os
-from collections import OrderedDict
-from urllib.parse import urljoin
 
-from felesatra.env import make_env
-from felesatra.resources.site import Website
-from felesatra.resources.site.atom import Author, Link
+import yaml
 
-
-def sitepath():
-    """Get path of website sources."""
-    return os.path.join(
-        os.path.dirname(__file__),
-        'site',
-    )
-
-
-def setup_env(env, site_url, src_dir):
-    """Set up environment."""
-    site_url = urljoin(site_url, '/')
-    env.globals['site'] = {
-        'url': site_url,
-        'srcdir': src_dir,
-        'nav': OrderedDict((
-            ('About', 'about/'),
-            ('Projects', 'projects/'),
-        )),
-    }
-    self_link = Link(urljoin(site_url, 'atom.xml'))
-    self_link.rel = 'self'
-    self_link.type = 'application/atom+xml'
-    env.globals['atom_context'] = {
-        'id': site_url,
-        'title': 'Feles Atra',
-        'links': [self_link],
-        'authors': [Author('Allen Li', site_url, None)],
-        'rights': 'Copyright 2010-2016 Allen Li',
-    }
+import frelia.enja
+import frelia.jinja
+import frelia.page
+import frelia.fs
 
 
 def main():
+    """Dance!"""
     logging.basicConfig(level='DEBUG')
+    args = parse_args()
+    frelia.fs.link_files(args.static_dir, args.build_dir)
+    globals_dict = load_globals(args.globals)
+    pages = list(load_pages(args.page_dir))
+    render_pages(globals_dict, pages, args.build_dir)
 
+
+def parse_args():
+    """Parse arguments."""
     parser = argparse.ArgumentParser()
     parser.add_argument('build_dir')
     parser.add_argument('--site-url', default='https://www.felesatra.moe')
-    args = parser.parse_args()
+    parser.add_argument('--static-dir', default='static')
+    parser.add_argument('--page-dir', default='pages')
+    parser.add_argument('--template-dir', default='templates')
+    parser.add_argument('--globals', default='globals.yaml')
+    return parser.parse_args()
 
-    src_dir = sitepath()
-    env = make_env()
-    setup_env(env, args.site_url, src_dir)
-    website = Website(src_dir)
-    website.index(env)
-    website.render(env, args.build_dir)
+
+def load_globals(filename):
+    with open(filename) as file:
+        return yaml.load(file, Loader=yaml.CLoader)
+
+
+def load_pages(page_dir):
+    page_loader = frelia.page.PageLoader(frelia.enja.EnjaDocument)
+    yield from page_loader.load_pages(page_dir)
+
+
+def render_pages(globals_dict, pages, build_dir):
+    env = frelia.jinja.Environment()
+    env.globals = globals_dict
+    document_renderer = frelia.page.JinjaDocumentRenderer(env)
+    page_renderer = frelia.page.PageRenderer(document_renderer, build_dir)
+    for page in pages:
+        page_renderer.render(page)
+
 
 if __name__ == '__main__':
     main()
