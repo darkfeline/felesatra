2b0021153d469f18809ffec13e7f20a45b2608e9
Allen Li <darkfeline@felesatra.moe>
2016-11-24 00:28:03 -0800

    Work

--- puppet/modules/felesatra/manifests/felesatra.pp
+++ puppet/modules/felesatra/manifests/felesatra.pp
@@ -1,3 +1,29 @@
 class felesatra::felesatra {
-  require felesatra::nginx
+  package { 'nginx':
+    ensure => latest,
+  }
+
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+    require => Package['nginx'],
+  }
+
+  $confdir = '/etc/nginx'
+
+  file { "${confdir}/sites-available/felesatra":
+    source => 'puppet:///modules/felesatra/nginx-sites/felesatra',
+    notify => Service['nginx'],
+  }
+
+  file { "${confdir}/sites-enabled/felesatra":
+    ensure => link,
+    target => '../sites-available/felesatra',
+    notify => Service['nginx'],
+  }
+
+  file { "${confdir}/sites-enabled/default":
+    ensure => missing,
+    notify => Service['nginx'],
+  }
 }
