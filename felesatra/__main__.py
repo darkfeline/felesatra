74e99fd40c70ece17eea81fbf9120ed45544b06f
Allen Li <darkfeline@felesatra.moe>
2016-11-25 16:37:08 -0800

    Move main

--- felesatra/__main__.py
+++ felesatra/__main__.py
@@ -1,12 +1,15 @@
 import argparse
 import logging
 import os
+import pathlib
 from collections import OrderedDict
 from urllib.parse import urljoin
 
 from felesatra.env import make_env
 from felesatra.resources.site import Website
 from felesatra.resources.site.atom import Author, Link
+import felesatra.resource.loaders
+import felesatra.resource.resources
 
 
 def sitepath():
@@ -51,9 +54,30 @@ def main():
     src_dir = sitepath()
     env = make_env()
     setup_env(env, args.site_url, src_dir)
-    website = Website(src_dir)
-    website.index(env)
-    website.render(env, args.build_dir)
+    # website = Website(src_dir)
+    # website.index(env)
+    # website.render(env, args.build_dir)
+
+    args.build_dir = pathlib.Path(args.build_dir)
+
+    loader = felesatra.resource.loaders.WebsiteLoader()
+    resources = list(loader(src_dir))
+
+    index_entries = [
+        resource.get_index_entry(env)
+        for resource in resources
+        if isinstance(resource, felesatra.resource.resources.Indexable)
+    ]
+    env.globals['page_index'] = index_entries
+
+    for resource in resources:
+        resource.deploy(env, args.build_dir)
+    sitemap = felesatra.resource.resources.SitemapResource('sitemap.xml')
+    sitemap.deploy(env, args.build_dir)
+    if 'atom_context' in env.globals:
+        atom = felesatra.resource.resources.AtomResource(
+            env.globals['atom_context'], 'atom.xml')
+        atom.deploy(env, args.build_dir)
 
 
 if __name__ == '__main__':
