9e03d0fd42f7360beccf9dbf7e383d645d343698
Allen Li <darkfeline@felesatra.moe>
2018-02-24 22:49:32 -0800

    Fix logging

--- app/app.py
+++ app/app.py
@@ -1,12 +1,14 @@
 import os
+import logging
 
 import flask
 from google.appengine.api.app_logging import AppLogsHandler
 
 _DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'pages')
 
+logging.getLogger().addHandler(AppLogsHandler())
+
 app = flask.Flask(__name__)
-app.logger.addHandler(AppLogsHandler())
 
 
 @app.route("/")
@@ -14,26 +16,30 @@ def index():
     return _load_page('index')
 
 
-@app.route('/<string:path>')
+@app.route('/<path:path>')
 def default(path):
-    try:
-        return _load_page(path)
-    except IOError as e:
-        app.logger.error('Error loading %s: %s', path, e)
-        flask.abort(404)
+    return _load_page(path)
 
 
-@app.route('/<string:path>/')
+@app.route('/<path:path>/')
 def trailing_slash(path):
     return flask.redirect('/' + path)
 
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return (_load_page('404'), 404)
+    return _load_page_unsafe('404'), 404
 
 
 def _load_page(path):
+    try:
+        return _load_page_unsafe(path)
+    except IOError as e:
+        logging.error('Error loading %s: %s', path, e)
+        flask.abort(404)
+
+
+def _load_page_unsafe(path):
     path = os.path.join(_DIR, path + '.html')
     with open(path) as f:
         return f.read()
