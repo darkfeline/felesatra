af1ca567c62854cac8c6862834bfe42b477540df
Allen Li <darkfeline@felesatra.moe>
2018-06-18 23:43:52 -0700

    Add anidb

--- go-app/pkg.go
+++ go-app/pkg.go
@@ -6,6 +6,7 @@ import (
 )
 
 var pkgs = []Package{
+	{"/anidb", Git, "https://github.com/darkfeline/anidb-go"},
 	{"/booru/dl", Git, "https://github.com/darkfeline/booru-dl-go"},
 	{"/dlsite", Git, "https://github.com/darkfeline/dlsite-go"},
 	{"/felesatra", Git, "https://github.com/darkfeline/felesatra"},
