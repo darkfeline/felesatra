0731da6231b73b561071f1f6703bb7d0100ac982
Allen Li <darkfeline@felesatra.moe>
2018-08-10 03:23:20 -0700

    Revert "Add booru"
    This reverts commit 51023e0ca2a999c20833786b8fc2e81cfb3b8d7d.

--- booru/templates.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"html/template"
-	"path/filepath"
-)
-
-var templatesDir = "templates"
-var templ = template.Must(template.ParseGlob(filepath.Join(templatesDir, "*")))
--- internal/booru/dstype/image.go
+++
@@ -1,5 +0,0 @@
-package dstype
-
-type Image struct {
-	Data []byte
-}
