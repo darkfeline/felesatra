0c41ed5d4d0e216dad52f12da9253454e2c667ef
Allen Li <darkfeline@felesatra.moe>
2019-02-02 02:09:40 -0800

    Regen

--- generator/internal/templates/templates.go
+++ generator/internal/templates/templates.go
@@ -4,9 +4,9 @@ import (
 	"text/template"
 )
 
-//go:generate binpack -var baseTemplate base.html
-//go:generate binpack -var sitemapTemplate sitemap.xml
-//go:generate binpack -var indexTemplate index.html
+//go:generate binpack -name baseTemplate base.html
+//go:generate binpack -name sitemapTemplate sitemap.xml
+//go:generate binpack -name indexTemplate index.html
 
 func LoadPageTemplate() *template.Template {
 	return template.Must(template.New("base.html").Parse(baseTemplate))
