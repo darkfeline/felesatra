8e0bf00711326db5308106ccfac26185b662b551
Allen Li <darkfeline@felesatra.moe>
2019-05-19 00:08:12 -0700

    Add mod packages

--- go/pkg.go
+++ go/pkg.go
@@ -10,16 +10,19 @@ var pkgs = []Package{
 	{"/animanager", Git, "https://github.com/darkfeline/animanager-go"},
 	{"/binpack", Git, "https://github.com/darkfeline/binpack"},
 	{"/booru/dl", Git, "https://github.com/darkfeline/booru-dl-go"},
-	{"/danbooru", Mod, "https://goproxy.felesatra.moe/"},
 	{"/dlsite", Git, "https://github.com/darkfeline/dlsite-go"},
 	{"/felesatra", Git, "https://github.com/darkfeline/felesatra"},
 	{"/go2/errors", Git, "https://github.com/darkfeline/go2-errors"},
 	{"/orbis", Git, "https://github.com/darkfeline/orbis-go"},
 	{"/pwnck", Git, "https://github.com/darkfeline/pwnck"},
-	{"/qualia", Git, "https://github.com/darkfeline/qualia-go"},
 	{"/sitemap", Git, "https://github.com/darkfeline/sitemap-go"},
 	{"/subcommands", Git, "https://github.com/darkfeline/subcommands-go"},
 	{"/xdg", Git, "https://github.com/darkfeline/xdg-go"},
+}
+
+var modulePkgs = []string{
+	"/danbooru",
+	"/qualia",
 }
 
 var pkgMap = make(map[string]*Package)
@@ -52,5 +55,13 @@ func findPackage(pp string) (p *Package, ok bool) {
 func init() {
 	for i, p := range pkgs {
 		pkgMap[p.Path] = &pkgs[i]
+	}
+	for _, path := range modulePkgs {
+		p := Package{
+			Path:   path,
+			Method: Mod,
+			URL:    "https://goproxy.felesatra.moe",
+		}
+		pkgMap[p.Path] = &p
 	}
 }
