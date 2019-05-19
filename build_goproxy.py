408b328a45e8d4ecaedf3432a2e2096b717016c1
Allen Li <darkfeline@felesatra.moe>
2019-05-18 23:52:40 -0700

    Add packages

--- build_goproxy.py
+++ build_goproxy.py
@@ -17,6 +17,20 @@ class Module:
 
 MODULES = [
     Module(name="go.felesatra.moe/danbooru", repo="https://github.com/darkfeline/danbooru-go"),
+    Module(name='go.felesatra.moe/anidb', repo='https://github.com/darkfeline/anidb-go'),
+    Module(name='go.felesatra.moe/animanager', repo='https://github.com/darkfeline/animanager-go'),
+    Module(name='go.felesatra.moe/binpack', repo='https://github.com/darkfeline/binpack'),
+    Module(name='go.felesatra.moe/booru/dl', repo='https://github.com/darkfeline/booru-dl-go'),
+    Module(name='go.felesatra.moe/danbooru', repo='https://goproxy.felesatra.moe/'),
+    Module(name='go.felesatra.moe/dlsite', repo='https://github.com/darkfeline/dlsite-go'),
+    Module(name='go.felesatra.moe/felesatra', repo='https://github.com/darkfeline/felesatra'),
+    Module(name='go.felesatra.moe/go2/errors', repo='https://github.com/darkfeline/go2-errors'),
+    Module(name='go.felesatra.moe/orbis', repo='https://github.com/darkfeline/orbis-go'),
+    Module(name='go.felesatra.moe/pwnck', repo='https://github.com/darkfeline/pwnck'),
+    Module(name='go.felesatra.moe/qualia', repo='https://github.com/darkfeline/qualia-go'),
+    Module(name='go.felesatra.moe/sitemap', repo='https://github.com/darkfeline/sitemap-go'),
+    Module(name='go.felesatra.moe/subcommands', repo='https://github.com/darkfeline/subcommands-go'),
+    Module(name='go.felesatra.moe/xdg', repo='https://github.com/darkfeline/xdg-go'),
 ]
 
 
@@ -42,11 +56,21 @@ def build_module(workdir: Path, outdir: Path, module: Module):
     versions = get_versions(checkout)
     logging.info(f'Got versions for module {module.name}: {versions}')
     base_path(outdir, module.name).mkdir(parents=True, exist_ok=True)
-    list_path(outdir, module.name).write_text(
-        ''.join(version + '\n' for version in versions))
+    good_versions = []
     for version in versions:
         logging.info(f'Building module {module.name} version {version}')
-        build_module_version(checkout, outdir, module.name, version)
+        try:
+            build_module_version(checkout, outdir, module.name, version)
+        except Exception as e:
+            logging.warning(f'Skipping module {module.name} version {version}: {e}')
+        else:
+            good_versions.append(version)
+    write_module_version_list(outdir, module.name, good_versions)
+
+
+def write_module_version_list(outdir: Path, module_name: str, versions: Iterable[str]):
+    list_path(outdir, module_name).write_text(
+        ''.join(version + '\n' for version in versions))
 
 
 def build_module_version(checkout: Path, outdir: Path, module_name: str, version: str):
@@ -62,6 +86,7 @@ def build_module_version(checkout: Path, outdir: Path, module_name: str, version
                        check=True, stdout=f)
 
 
+# Basic Git operations
 def get_versions(checkout: Path) -> List[str]:
     p = subprocess.run(['git', '-C', checkout, 'tag'], capture_output=True, check=True)
     tags = p.stdout.decode().splitlines()
@@ -80,6 +105,7 @@ def get_mod_file(checkout: Path, ref: str) -> str:
     return p.stdout.decode()
 
 
+# Paths
 def base_path(outdir: Path, module_name: str) -> Path:
     return outdir / module_name / '@v'
 
