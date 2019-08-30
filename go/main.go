88a3fa0d781a63930873103e8d0991ce4e8a1158
Allen Li <darkfeline@felesatra.moe>
2019-08-30 16:21:16 -0700

    Simplify

--- go/main.go
+++ go/main.go
@@ -36,31 +36,9 @@ var temp = template.Must(template.New("go").Parse(
 func handle(w http.ResponseWriter, r *http.Request) {
 	p, ok := findPackage(r.URL.Path)
 	if !ok {
-		write404(w)
+		w.WriteHeader(404)
+		_, _ = w.Write([]byte("Package not found"))
 		return
 	}
-	err := temp.Execute(w, p)
-	if err != nil {
-		log.Printf("Error writing response: %s", err)
-		serverError(w)
-		return
-	}
-}
-
-// write404 writes a 404 response.
-func write404(w http.ResponseWriter) {
-	w.WriteHeader(404)
-	_, err := w.Write([]byte("Package not found"))
-	if err != nil {
-		log.Printf("Error sending server error: %s", err)
-	}
-}
-
-// serverError writes a generic 500 response.
-func serverError(w http.ResponseWriter) {
-	w.WriteHeader(500)
-	_, err := w.Write([]byte("Error"))
-	if err != nil {
-		log.Printf("Error sending server error: %s", err)
-	}
+	_ = temp.Execute(w, p)
 }
