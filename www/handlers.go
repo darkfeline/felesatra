e458e05e9b35a29b310b9154df558d674e1d5499
Allen Li <darkfeline@felesatra.moe>
2019-12-31 20:09:48 -0800

    Add caching

---
+++ www/handlers.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
+
+func newPublicHandler() http.Handler {
+	return publicHandler{
+		fs: http.Dir("pages"),
+	}
+}
+
+type publicHandler struct {
+	fs http.FileSystem
+}
+
+func (h publicHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	p := req.URL.Path
+	if p == "/" {
+		p = "index"
+	}
+	if strings.HasSuffix(p, "/") {
+		writeRedirect(w, strings.TrimRight(p, "/"))
+		return
+	}
+	p = p + ".html"
+	f, err := h.fs.Open(p)
+	if err != nil {
+		if os.IsNotExist(err) {
+			write404(w)
+			return
+		}
+		log.Printf("Public handler error: %s", err)
+		http.Error(w, "Server Error", 500)
+		return
+	}
+	w = cacheWriter{
+		ResponseWriter: w,
+		// Cache for one week.
+		cacheControl: []string{"public, max-age=604800"},
+	}
+	http.ServeContent(w, req, "foo.html", time.Time{}, f)
+	return
+}
+
+func writeRedirect(w http.ResponseWriter, p string) {
+	h := w.Header()
+	h["Location"] = []string{p}
+	w.WriteHeader(301)
+}
+
+// write404 writes a 404 response.
+func write404(w http.ResponseWriter) {
+	d, err := ioutil.ReadFile("pages/404.html")
+	if err != nil {
+		log.Printf("Error reading 404 page: %s", err)
+		d = []byte("Page not found")
+	}
+	w.WriteHeader(404)
+	w.Write(d)
+}
+
+func newPrivateFileServer() http.Handler {
+	return privateHandler{
+		fs: http.FileServer(http.Dir("private")),
+	}
+}
+
+type privateHandler struct {
+	fs http.Handler
+}
+
+func (h privateHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	u, pw, ok := req.BasicAuth()
+	if !ok {
+		writeBasicAuth(w)
+		return
+	}
+	// checkAuth function needs to be defined separately.
+	if !checkAuth(u, pw) {
+		writeBasicAuth(w)
+		return
+	}
+	w = cacheWriter{
+		ResponseWriter: w,
+		// Cache for one day.
+		cacheControl: []string{"private, max-age=86400"},
+	}
+	h.fs.ServeHTTP(w, req)
+}
+
+func writeBasicAuth(w http.ResponseWriter) {
+	h := w.Header()
+	h["WWW-Authenticate"] = []string{"Basic realm=\"yggdrasil\""}
+	w.WriteHeader(401)
+}
+
+// cacheWriter implements a http.ResponseWriter that adds a
+// Cache-Control header when writing 200 OK responses.
+type cacheWriter struct {
+	http.ResponseWriter
+	cacheControl []string
+}
+
+func (w cacheWriter) WriteHeader(c int) {
+	if c == http.StatusOK {
+		h := w.ResponseWriter.Header()
+		if _, ok := h["Cache-Control"]; !ok {
+			h["Cache-Control"] = w.cacheControl
+		}
+	}
+	w.ResponseWriter.WriteHeader(c)
+}
--- www/main.go
+++ www/main.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	"strings"
 	"time"
 )
 
@@ -18,7 +16,7 @@ func main() {
 	}
 	log.Printf("Listening on port %s", port)
 	m := http.NewServeMux()
-	m.HandleFunc("/", wrapError(handlePublic))
+	m.Handle("/", newPublicHandler())
 	m.Handle("/private/", http.StripPrefix("/private/", newPrivateFileServer()))
 	s := &http.Server{
 		Addr:           fmt.Sprintf(":%s", port),
@@ -28,83 +26,4 @@ func main() {
 		MaxHeaderBytes: 1 << 18, // 256 KiB
 	}
 	log.Fatal(s.ListenAndServe())
-}
-
-func handlePublic(w http.ResponseWriter, r *http.Request) error {
-	p := r.URL.Path
-	if p == "/" {
-		p = "index"
-	}
-	if strings.HasSuffix(p, "/") {
-		h := w.Header()
-		h["Location"] = []string{strings.TrimRight(p, "/")}
-		w.WriteHeader(301)
-		return nil
-	}
-	d, err := readPage(p)
-	if err != nil {
-		if os.IsNotExist(err) {
-			return write404(w)
-		}
-		return fmt.Errorf("Error reading page: %s", err)
-	}
-	w.Write(d)
-	return nil
-}
-
-// write404 writes a 404 response.
-func write404(w http.ResponseWriter) error {
-	d, err := readPage("404")
-	if err != nil {
-		return fmt.Errorf("Error reading 404 page: %s", err)
-	}
-	w.WriteHeader(404)
-	w.Write(d)
-	return nil
-}
-
-type handlerFunc func(http.ResponseWriter, *http.Request)
-type handlerFuncE func(http.ResponseWriter, *http.Request) error
-
-func wrapError(f handlerFuncE) handlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		if err := f(w, r); err != nil {
-			log.Printf("Handler error: %s", err)
-			http.Error(w, "Server Error", 500)
-		}
-	}
-}
-
-func readPage(p string) ([]byte, error) {
-	return ioutil.ReadFile(fmt.Sprintf("pages/%s.html", p))
-}
-
-func newPrivateFileServer() authHandler {
-	return authHandler{
-		h: http.FileServer(http.Dir("private")),
-	}
-}
-
-type authHandler struct {
-	h http.Handler
-}
-
-func (s authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	u, pw, ok := r.BasicAuth()
-	if !ok {
-		writeBasicAuth(w)
-		return
-	}
-	if !checkAuth(u, pw) {
-		writeBasicAuth(w)
-		return
-	}
-	s.h.ServeHTTP(w, r)
-	return
-}
-
-func writeBasicAuth(w http.ResponseWriter) {
-	h := w.Header()
-	h["WWW-Authenticate"] = []string{"Basic realm=\"yggdrasil\""}
-	w.WriteHeader(401)
 }
