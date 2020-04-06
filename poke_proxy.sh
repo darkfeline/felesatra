80135465b9b89343c82b03e96968024b25e58251
Allen Li <darkfeline@felesatra.moe>
2020-04-05 17:44:46 -0700

    Fix

--- poke_proxy.sh
+++ poke_proxy.sh
@@ -1,5 +1,5 @@
 set -eu
 cd app/srv/goproxy || exit 1
 readonly host=https://proxy.golang.org
-find * -name "*.info" -printf "-s ${host}/%p\n" | xargs -P 0 -L 1 curl
+find * -name "*.info" -printf "-s ${host}/%p\n" | xargs -P 0 -L 1 curl -w '\n'
 echo
