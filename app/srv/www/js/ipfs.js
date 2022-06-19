634ddc46dd5e7bf7a4389443a35343f7219f48d9
Allen Li <darkfeline@felesatra.moe>
2022-06-18 20:06:26 -0700

    Add comment to helper

--- app/srv/www/js/ipfs.js
+++ app/srv/www/js/ipfs.js
@@ -1,3 +1,10 @@
+// This helper rewrites links to work on IPFS.
+//
+// The site normally serves page resources without an extension (in
+// theory, resources could be served in alternate representations).
+//
+// However, IPFS can't really handle this, so we dynamically rewrite
+// links to add a .html extension.
 (function(){
   // Only run if this is on IPFS.
   if (!window.location.href.includes("ipfs")) {
