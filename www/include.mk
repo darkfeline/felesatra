9766f2f8cfea6685480eee35fc849af6c07a6237
Allen Li <darkfeline@felesatra.moe>
2023-10-21 01:56:52 -0700

    Clean www local_dir

--- www/include.mk
+++ www/include.mk
@@ -1,40 +1,36 @@
-local_dir := www
-
-srv_www_dir := $(srvdir)/www
-
-www_pages_dir := $(local_dir)/pages
+www_pages_dir := www/pages
 www_pages := $(shell find $(www_pages_dir) -name '*.html')
 
-clean_paths += $(local_dir)/index
-$(local_dir)/index: $(www_pages) $(kanade_path)
+clean_paths += www/index
+www/index: $(www_pages) $(kanade_path)
 	$(kanade_path) index-pages $(www_pages_dir) > $@
 
-clean_paths += $(local_dir)/index.html
-$(local_dir)/index.html: $(local_dir)/index $(kanade_path)
+clean_paths += www/index.html
+www/index.html: www/index $(kanade_path)
 	$(kanade_path) make-index-page $< > $@
 
-clean_paths += $(srv_www_dir)/index.html
-all: $(srv_www_dir)/index.html
-$(srv_www_dir)/index.html: $(local_dir)/index.html $(kanade_path)
+clean_paths += $(srvdir)/www/index.html
+all: $(srvdir)/www/index.html
+$(srvdir)/www/index.html: www/index.html $(kanade_path)
 	$(kanade_path) render $< > $@
 
-clean_paths += $(srv_www_dir)/404.html
-all: $(srv_www_dir)/404.html
-$(srv_www_dir)/404.html: $(local_dir)/pages/404.html $(kanade_path)
+clean_paths += $(srvdir)/www/404.html
+all: $(srvdir)/www/404.html
+$(srvdir)/www/404.html: www/pages/404.html $(kanade_path)
 	$(kanade_path) render $< > $@
 
-clean_paths += $(srv_www_dir)/atom.xml
-all: $(srv_www_dir)/atom.xml
-$(srv_www_dir)/atom.xml: $(local_dir)/index $(kanade_path)
+clean_paths += $(srvdir)/www/atom.xml
+all: $(srvdir)/www/atom.xml
+$(srvdir)/www/atom.xml: www/index $(kanade_path)
 	$(kanade_path) make-atom-feed $< > $@
 
-clean_paths += $(srv_www_dir)/sitemap.xml
-all: $(srv_www_dir)/sitemap.xml
-$(srv_www_dir)/sitemap.xml: $(local_dir)/index $(kanade_path)
+clean_paths += $(srvdir)/www/sitemap.xml
+all: $(srvdir)/www/sitemap.xml
+$(srvdir)/www/sitemap.xml: www/index $(kanade_path)
 	$(kanade_path) make-sitemap $< > $@
 
-include $(local_dir)/render.mk
+include www/render.mk
 
-clean_paths += $(local_dir)/render.mk
-$(local_dir)/render.mk: $(local_dir)/index $(kanade_path)
+clean_paths += www/render.mk
+www/render.mk: www/index $(kanade_path)
 	$(kanade_path) make-render-makefile $< > $@
